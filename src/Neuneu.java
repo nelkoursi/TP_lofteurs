3617df6d8a40a3b098186701a1bc80dc30c6ea87
nelkoursi <nora.el-koursi@eleves.ec-nantes.fr>
2012-10-01 17:38:36 +0200

    Création des classes Neuneu et Case. La classe Neuneu est vide. La classe Case a tous ses attributs de créés, et les getter et setter d'abscisse et ordonnee sont créés. Il reste le constructeur puis les méthodes à faire.

--- src/Neuneu.java
+++ src/Neuneu.java
@@ -7,11 +7,16 @@
  * @author Nora
  *
  */
-public class ClasseTest {
+public abstract class Neuneu {
 
 	/**
-	 * @param args
+	 * @param energie du Neuneu
 	 */
+	protected int energieNeuneu;
+	protected Case position;
+	
+	
+	
 	public static void main(String[] args) {
 		// TODO Stub de la méthode généré automatiquement
 
