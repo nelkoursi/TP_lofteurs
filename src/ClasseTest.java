e89b40d5288e1832ba73f27bb61389fcd227c1aa
Nora <Nora@130.66.207.35>
2012-10-01 17:10:06 +0200

    Test pour vérifier que le commit marche. Les classes sont vides.

---
+++ src/ClasseTest.java
@@ -0,0 +1,20 @@
+/**
+ * 1 oct. 2012
+ *TP_lofteurs	 
+ */
+
+/**
+ * @author Nora
+ *
+ */
+public class ClasseTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Stub de la méthode généré automatiquement
+
+	}
+
+}
